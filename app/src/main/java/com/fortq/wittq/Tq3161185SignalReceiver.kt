ffd94af181add3bb065c4c6d5e4aa66baf38374b
장부식 [bsjang] <bsjang@nexon.co.kr>
2026-03-17 19:16:10 +0900

    fix: remove duplicate Tq3161185SignalWidgetReceiver class in SignalReceiver.kt

--- app/src/main/java/com/fortq/wittq/Tq3161185SignalReceiver.kt
+++
@@ -1,8 +0,0 @@
-﻿package com.fortq.wittq
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class Tq3161185SignalWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = Tq3161185SignalWidget()
-}
